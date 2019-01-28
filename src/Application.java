56020d7879d903185b0b15e05eae5bf8a418116c
GareevaFarida <gareeva.farida@gmail.com>
2019-01-29 00:46:52 +0300

    1. Перенесла формирование сообщения об ошибке в исключение. 2. Добавила условие на увеличение общей суммы только в случае отсутствия ошибок в данных массива.

--- src/Application.java
+++ src/Application.java
@@ -13,45 +13,47 @@ public class Application {
         int y = Dimentions.Y.getValue();
 
         if (massive.length != x) {
-            throw new MyArraySizeException("В метод передан массив с несоответствующей размерностью массива. Количество строк в массиве: " + massive.length + " вместо " + x);
+            throw new MyArraySizeException(massive.length, x);
         }
 
         //проверяем каждую строку массива на правильность количества элементов
         for (int i = 0; i < x; i++) {
             if (massive[i].length != y) {
-                throw new MyArraySizeException("В метод передан массив с несоответствующей размерностью массива. Количество элементов в строке с индексом: " + i + " составляет " + massive[i].length + " вместо " + y);
+                throw new MyArraySizeException(i, massive[i].length, y);
             }
         }
-            // System.out.println("Размер переданного массива соответствует контрольным значениям");
+        // System.out.println("Размер переданного массива соответствует контрольным значениям");
 
-            //Задание 2.
-            //создаем массив для целочисленных значений, полученных в результате преобразования строки
-            int[][] massiveInt = new int[x][y];
+        //Задание 2.
+        //создаем массив для целочисленных значений, полученных в результате преобразования строки
+        int[][] massiveInt = new int[x][y];
 
-            boolean dataError = false;
-            String message_error = "";
+        boolean dataError = false;
+        String message_error = "";
 
-            int summa = 0;
+        int summa = 0;
 
-            //преобразование символов в целые числа
-            for (int i = 0; i < x; i++) {
-                for (int j = 0; j < y; j++) {
-                    try {
-                        massiveInt[i][j] = Integer.parseInt(massive[i][j]);
+        //преобразование символов в целые числа
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                try {
+                    massiveInt[i][j] = Integer.parseInt(massive[i][j]);
+                    if (!dataError) {
                         summa = summa + massiveInt[i][j];
-                    } catch (NumberFormatException e) {
-                        message_error = message_error + "\n" +
-                                "Ячейка [" + i + "][" + j + "] содержит неверные данные: " + massive[i][j] + ". Невозможно преобразовать в числовой формат.";
-                        dataError = true;
                     }
+                } catch (NumberFormatException e) {
+                    message_error = message_error + "\n" +
+                            "Ячейка [" + i + "][" + j + "] содержит неверные данные: " + massive[i][j] + ". Невозможно преобразовать в числовой формат.";
+                    dataError = true;
                 }
             }
-            if (dataError) {
-                throw new MyArrayDataException(message_error);
-            }
-
-            return summa;
+        }
+        if (dataError) {
+            throw new MyArrayDataException(message_error);
         }
 
+        return summa;
     }
+
+}
 
