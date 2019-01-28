cbc7fc0cca2b4e473a095b752b542401133f20c2
GareevaFarida <gareeva.farida@gmail.com>
2019-01-29 00:43:17 +0300

    Перенесла формирование сообщения об ошибке в параметризованный конструктор исключения

--- src/Exceptions/MyArraySizeException.java
+++ src/Exceptions/MyArraySizeException.java
@@ -7,4 +7,12 @@ public class MyArraySizeException extends Exception {
     public MyArraySizeException(String message) {
         super(message);
     }
+
+    public MyArraySizeException(int RowsCount,int StandartRowsCount){
+        super("В метод передан массив с несоответствующей размерностью массива. Количество строк в массиве: " + RowsCount + " вместо " + StandartRowsCount);
+    }
+
+    public  MyArraySizeException(int RowIndex,int RowLength,int StandartRowLength) {
+        super("В метод передан массив с несоответствующей размерностью массива. Количество элементов в строке с индексом: " + RowIndex + " составляет " + RowLength + " вместо " + StandartRowLength);
+    }
 }
