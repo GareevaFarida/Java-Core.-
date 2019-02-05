d36f2fb9d9396a63c51fd11ccee831350aab7555
GareevaFarida <gareeva.farida@gmail.com>
2019-02-05 10:49:08 +0300

    Домашняя работа по уроку 4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        Application application = new Application();
-        application.Task1();
-        application.Task2();
-
+        new MyWindow();
     }
 }
