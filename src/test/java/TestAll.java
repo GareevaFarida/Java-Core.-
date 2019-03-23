f3add1ed2c2f2a903fa52e9d0db5f38f73d12426
GareevaFarida <gareeva.farida@gmail.com>
2019-03-23 23:52:07 +0300

    Практическое задание 6. Задания 1 и 2 (тесты).

---
+++ src/test/java/TestAll.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestTask1.class,
+        TestTask2.class
+})
+public class TestAll {
+}
